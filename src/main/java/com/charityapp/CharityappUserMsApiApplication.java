04156fee9922e940aed8a2dca90e51926c9ccd13
Monisha Govindaraju <rg.monisha105@gmail.com>
2022-05-05 17:45:23 +0530

    CHARITY APP USER

--- src/main/java/com/charityapp/CharityappUserMsApiApplication.java
+++ src/main/java/com/charityapp/CharityappUserMsApiApplication.java
@@ -2,12 +2,14 @@ package com.charityapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CharityappUserMsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CharityappUserMsApiApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
